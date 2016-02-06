83ba5ef5cd3865d2cab3a2775685f402abd9f019
Mike Lewis <mikelikespie@gmail.com>
2016-02-06 13:43:06 -0800

    Adding test to test accurracy of colors if using raw mapping

--- Sources/OPC/Colors.swift
+++ Sources/OPC/Colors.swift
@@ -20,8 +20,19 @@ private func clampUnit(x: Float) -> Float {
     return x
 }
 
+public struct RGBFloatDistance {
+    public var r, g, b: Float
+    
+    public var magnitude: Float {
+        return sqrt(magnitudeSquared)
+    }
+    public var magnitudeSquared: Float {
+        return r * r + g * g + b * b
+    }
+}
+
 /// Represent colors on unit scale
-public struct RGBFloat : ColorConvertible {
+public struct RGBFloat : ColorConvertible, CustomStringConvertible {
     // Must be set between 0 and 1. Is not checked
     public var r, g, b: Float
     
@@ -37,6 +48,100 @@ public struct RGBFloat : ColorConvertible {
         self.r = r
         self.g = g
         self.b = b
+    }
+    
+    
+    public func gammaAdjusted(gamma: Float = 2) -> RGBFloat {
+        if gamma == 2 {
+            return RGBFloat(r: r * r, g: g * g, b: b * b)
+        }
+        return RGBFloat(r: pow(r, gamma), g: pow(g, gamma), b: pow(b, gamma))
+    }
+    
+    /// Should convert us to closest match of RGBARawColor
+    public var rawColor: RGBARaw {
+        
+        // Possible naive algorithm
+        
+        
+        let minComponent = min(r, min(g,b))
+        let maxComponent = max(r, max(g,b))
+        
+        // This is the smallest our alpha can be
+        let minA = UInt8(maxComponent * 31)
+        let maxA: UInt8
+        
+        if maxComponent < 1.0 / 255.0 {
+            maxA = UInt8(maxComponent / 255 * 31)
+        } else {
+            maxA = 31
+        }
+        
+        let alphaSearchRange = minA...maxA
+        
+        var closestDeltaSquared = Float.infinity
+        var closestRGBAColor = RGBARaw()
+            
+        /// If we have a delta less than 0.001, we can exit
+        let thresholdSquared: Float = 0.001 * 0.001
+        let secondThresholdSquared: Float = 0.00175 * 0.00175
+        
+        for a in alphaSearchRange {
+            
+            let c = self.closestRawColorWithAlpha(a)
+            
+            let deltaSquared = self.distanceTo(c.rgbFloat).magnitudeSquared
+            
+            
+            let isMin = deltaSquared < closestDeltaSquared
+            if isMin {
+                closestRGBAColor = c
+                closestDeltaSquared = deltaSquared
+            }
+            
+            let numSearched = a - minA
+
+            
+            if numSearched > 5 {
+                if closestDeltaSquared < secondThresholdSquared {
+                    break
+                }
+            } else if numSearched > 2 && closestDeltaSquared < thresholdSquared {
+                break
+            }
+        }
+        
+        return closestRGBAColor
+    }
+    
+    private func closestRawColorWithAlpha(alpha: UInt8) -> RGBARaw {
+        if alpha == 0 {
+            return RGBARaw(r: 0, g: 0, b: 0, a: 0)
+        }
+        
+        let alphaFloat = Float(alpha) / 31
+        
+        // for each channel, solve c_256 in
+        //   c_float = alpha_32 / 31 * c_256 / 256
+        //   c_float = (c_256 / 255) * alpha_float
+        //   c_float / alpha_float = (c_256 / 255)
+        //   c_float / alpha_float * 255 = c_256
+        
+        /// TODO: see if rounding works better?
+        return RGBARaw(
+            r: UInt8(min(r / alphaFloat, 1) * 255),
+            g: UInt8(min(g / alphaFloat, 1) * 255),
+            b: UInt8(min(b / alphaFloat, 1) * 255),
+            a: alpha
+        )
+    }
+    
+    public func distanceTo(other: RGBFloat) -> RGBFloatDistance {
+        return RGBFloatDistance(r: other.r - r, g: other.g - g, b: other.b - b)
+    }
+    
+    public var description: String {
+        return "RGBFloat(r: \(r), g: \(g), b: \(b))"
     }
 }
 
@@ -59,14 +164,47 @@ public struct RGB8 : ColorConvertible {
         self.g = g
         self.b = b
     }
-    
-    
 }
 
 public protocol ColorConvertible {
-    var rgb8: RGB8 { get }
     var rgbFloat: RGBFloat { get }
 }
+
+public extension ColorConvertible {
+    /// Default implementation
+    var rgb8: RGB8 {
+        return self.rgbFloat.rgb8
+    }
+}
+
+
+/// Represents the color format for an APA102
+/// a is 0-31
+public struct RGBARaw : ColorConvertible, CustomStringConvertible {
+    public var r: UInt8
+    public var g: UInt8
+    public var b: UInt8
+    public var a: UInt8
+    
+    public var rgbFloat: RGBFloat {
+        let aFloat = Float(a) / 31
+        return RGBFloat(r: Float(r) / 255.0 * aFloat, g: Float(g) / 255.0 * aFloat, b: Float(b) / 255.0 * aFloat)
+    }
+
+    public init(r: UInt8 = 0, g: UInt8 = 0, b: UInt8 = 0, a: UInt8 = 31) {
+        assert(a < 32)
+        
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+    }
+    
+    public var description: String {
+        return "RGBARaw(r: \(r), g: \(g), b: \(b), a: \(a))"
+    }
+}
+
 
 public struct HSV : ColorConvertible {
     public var h: Float
@@ -148,77 +286,6 @@ public struct HSV : ColorConvertible {
         }
         return RGBFloat(r: r, g: g, b: b)
     }
-    
-    public var rgb8: RGB8 {
-        // Transliterated from FastLED
-        let invsat = 1.0 - s
-        let brightness_floor = (v * invsat)
-        let color_amplitude = v - brightness_floor
-        
-        // Figure out which section of the hue wheel we're in,
-        // and how far offset we are withing that section
-        let section = Int(floor(h * 3.0))
-        let offset = (h * 3.0) % 1.0
-        
-        let rampup = offset
-        let rampdown = 1.0 - offset
-        
-        // We now scale rampup and rampdown to a 0-255 range -- at least
-        // in theory, but here's where architecture-specific decsions
-        // come in to play:
-        // To scale them up to 0-255, we'd want to multiply by 4.
-        // But in the very next step, we multiply the ramps by other
-        // values and then divide the resulting product by 256.
-        // So which is faster?
-        //   ((ramp * 4) * othervalue) / 256
-        // or
-        //   ((ramp    ) * othervalue) /  64
-        // It depends on your processor architecture.
-        // On 8-bit AVR, the "/ 256" is just a one-cycle register move,
-        // but the "/ 64" might be a multicycle shift process. So on AVR
-        // it's faster do multiply the ramp values by four, and then
-        // divide by 256.
-        // On ARM, the "/ 256" and "/ 64" are one cycle each, so it's
-        // faster to NOT multiply the ramp values by four, and just to
-        // divide the resulting product by 64 (instead of 256).
-        // Moral of the story: trust your profiler, not your insticts.
-        
-        // Since there's an AVR assembly version elsewhere, we'll
-        // assume what we're on an architecture where any number of
-        // bit shifts has roughly the same cost, and we'll remove the
-        // redundant math at the source level:
-        
-        //  // scale up to 255 range
-        //  //rampup *= 4; // 0..252
-        //  //rampdown *= 4; // 0..252
-        
-        // compute color-amplitude-scaled-down versions of rampup and rampdown
-        let rampup_amp_adj   = (rampup   * color_amplitude)
-        let rampdown_amp_adj = (rampdown * color_amplitude)
-        
-        // add brightness_floor offset to everything
-        
-        let r, g, b: UInt8
-        
-        
-        switch section {
-        case 0:
-            r = UInt8(brightness_floor * 255)
-            g = UInt8(rampdown_amp_adj * 255)
-            b = UInt8(rampup_amp_adj * 255)
-        case 1:
-            r = UInt8(rampup_amp_adj * 255)
-            g = UInt8(brightness_floor * 255)
-            b = UInt8(rampdown_amp_adj * 255)
-        case 2:
-            r = UInt8(rampdown_amp_adj * 255)
-            g = UInt8(rampup_amp_adj * 255)
-            b = UInt8(brightness_floor * 255)
-        default: preconditionFailure()
-        }
-        return RGB8(r: r, g: g, b: b)
-    }
-    
 }
 
 
---
+++ SwiftledMobile/OPCTests/ColorsTest.swift
@@ -0,0 +1,114 @@
+//
+//  OPCTests.swift
+//  OPCTests
+//
+//  Created by Michael Lewis on 2/6/16.
+//  Copyright © 2016 Lolrus Industries. All rights reserved.
+//
+
+import XCTest
+
+import OPC
+//
+//public struct RGBFloatDistance {
+//    var r,g,b: Float
+//}
+//
+//public extension RGBFloat: Strideable {
+//
+//}
+//
+//public protocol FloatDivisible {
+//    func /(lhs: Self, rhs: Float) -> Self
+//}
+//
+//public extension ClosedInterval where Bound: Strideable {
+//
+//}
+
+class OPCTests: XCTestCase {
+    let colorsToTest: [RGBFloat] = {
+        let steps = 100
+        let stepsFloat = Float(steps)
+        
+        /// Go all the way from 0 to 1
+        let stepsInterpolated = (0...steps).map { Float($0) / stepsFloat }
+        
+        return stepsInterpolated.flatMap { r in
+            stepsInterpolated.flatMap { g in
+                stepsInterpolated.map { b in
+                    // Gamma adjust the colors to make them more on the log scale
+                    RGBFloat(r: r, g: g, b: b).gammaAdjusted()
+                }
+            }
+        }
+    }()
+    
+    let dimColorsToTest: [RGBFloat] = {
+        let steps = 100
+        let stepsFloat = Float(steps)
+        
+        /// Go all the way from 0 to 1
+        let stepsInterpolated = (0...steps).map { Float($0) / stepsFloat * 0.5 }
+        
+        return stepsInterpolated.flatMap { r in
+            stepsInterpolated.flatMap { g in
+                stepsInterpolated.map { b in
+                    // Gamma adjust the colors to make them more on the log scale
+                    RGBFloat(r: r, g: g, b: b).gammaAdjusted()
+                }
+            }
+        }
+    }()
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    
+    func testMapping<C: ColorConvertible>(name: String, colors: [RGBFloat], mappingFunc: (input: RGBFloat) -> C) {
+        // This is an example of a performance test case.
+        var deltaSum: Double = 0
+        var logSum: Double = 0
+        var count = 0
+        
+        var maxAdjustedDelta = 0.0
+        var maxDelta = 0.0
+        
+        for c in colors {
+            let mapping = mappingFunc(input: c)
+            let adjustedDelta = Double(mapping.rgbFloat.gammaAdjusted(0.5).distanceTo(c.gammaAdjusted(0.5)).magnitude)
+            let delta = Double(mapping.rgbFloat.distanceTo(c).magnitude)
+            
+            deltaSum += adjustedDelta
+            logSum += log(adjustedDelta + 0.0001)
+            if c.r != 1 && c.g != 1 && c.b != 1 {
+                maxAdjustedDelta = max(maxAdjustedDelta, adjustedDelta)
+                maxDelta = max(maxDelta, delta)
+            }
+            count++
+        }
+        
+        
+        print("\(name): mean of delta: \(deltaSum / Double(count))")
+        print("\(name): geomMean of delta: \(exp(logSum / Double(count)))")
+        print("\(name): maxAdjustedDelta: \(maxAdjustedDelta)")
+        print("\(name): maxDelta: \(maxDelta)")
+        
+        self.measureBlock {
+            for c in self.colorsToTest {
+                mappingFunc(input: c)
+            }
+        }
+        
+    }
+    
+    func testRGB8ColorMapping() {
+        testMapping("rgb8", colors: dimColorsToTest) { $0.rgb8 }
+    }
+    
+    func testRawColorMapping() {
+        testMapping("rgbRaw", colors: dimColorsToTest) { $0.rawColor }
+    }
+}
