e17b321176ba98f2b2c95bdc5c5be1da043281b1
Mike Lewis <mikelikespie@gmail.com>
2016-07-20 09:03:32 -0700

    wip

--- Sources/OPC/Net.swift
+++ Sources/OPC/Net.swift
@@ -188,13 +188,15 @@ public func getaddrinfoSockAddrsAsync(_ hostname: String, servname: String, work
                         continue
                     }
                     
+                    let socktype = __socket_type(UInt32(aiMem.ai_socktype))!
+                    
                     switch aiMem.ai_addr.pointee.sa_family {
                     case UInt16(AF_INET):
                         let addr = unsafeBitCast(curAi?.pointee.ai_addr, to: UnsafePointer<sockaddr_in>.self).pointee
-                        observer.onNext(AddrInfo(family: aiMem.ai_family, socktype: aiMem.ai_socktype.rawValue, proto: aiMem.ai_protocol, addr: addr))
+                        observer.onNext(AddrInfo(family: aiMem.ai_family, socktype: socktype, proto: aiMem.ai_protocol, addr: addr))
                     case UInt16(AF_INET6):
                         let addr = unsafeBitCast(curAi?.pointee.ai_addr, to: UnsafePointer<sockaddr_in6>.self).pointee
-                        observer.onNext(AddrInfo(family: aiMem.ai_family, socktype: aiMem.ai_socktype.rawValue, proto: aiMem.ai_protocol, addr: addr))
+                        observer.onNext(AddrInfo(family: aiMem.ai_family, socktype: socktype, proto: aiMem.ai_protocol, addr: addr))
                     default:
                         NSLog("skiping")
                         continue
